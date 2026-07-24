c65f68a5742878dc2381a513adb8ef2ac15624cb
Gazi Seeyam <hellogaziseeyam@gmail.com>
2026-07-25 05:04:33 +0600

    Test ImageResponse

---
+++ app/api/day-wallpaper/route.tsx
@@ -0,0 +1,26 @@
+import { ImageResponse } from "@vercel/og";
+
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          background: "red",
+          color: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: 80,
+        }}
+      >
+        TEST
+      </div>
+    ),
+    {
+      width: 937,
+      height: 1678,
+    }
+  );
+}
