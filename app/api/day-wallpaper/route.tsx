54af1b48a1d4c0848b09b77d68f535a2fa6c645f
Gazi Seeyam <hellogaziseeyam@gmail.com>
2026-07-25 05:11:13 +0600

    Wallpaper test

--- app/api/day-wallpaper/route.tsx
+++ app/api/day-wallpaper/route.tsx
@@ -5,20 +5,37 @@ export const runtime = "edge";
 export async function GET() {
   return new ImageResponse(
     (
-      <div
-        style={{
-          width: "100%",
-          height: "100%",
-          background: "red",
-          color: "white",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: 80,
-        }}
-      >
-        TEST
-      </div>
+     <div
+  style={{
+    width: "100%",
+    height: "100%",
+    position: "relative",
+    display: "flex",
+  }}
+>
+  <img
+    src="https://quran-wallpaper.vercel.app/day.png"
+    style={{
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+      position: "absolute",
+    }}
+  />
+
+  <div
+    style={{
+      position: "absolute",
+      width: "100%",
+      top: 450,
+      color: "white",
+      textAlign: "center",
+      fontSize: 60,
+    }}
+  >
+    TEST
+  </div>
+</div>
     ),
     {
       width: 937,
